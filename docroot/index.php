f2bcf52b322b25dd93eec499e1db7a90d32793e1
Michael Donnelly <MichaelVDonnelly@gmail.com>
2014-08-04 16:50:27 +0000

    Initial (static) php index.

---
+++ docroot/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>: Nerd Pub Trivia ::  :</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href="http://fonts.googleapis.com/css?family=VT323" rel="stylesheet" type="text/css" />
+    <link href="/style/site-style.css" type="text/css" rel="stylesheet" />
+    <style>
+      body {
+      font-size: 32px;
+      }
+      h1 {
+      font-size: 64px;
+      }
+      div#intro img {
+      float: right;
+      clear: left;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="intro">
+      <h1>Nerd Pub Trivia!</h1>
+      <img src="/images/male-nerd.png" alt="Nerd Boy!"><img src="/images/female-nerd.png" alt="Nerd Girl!">
+      <p>
+        Do you wish that pub trivia asked questions about subjects you care about, like science, books &amp; math? Wish no more,
+        because we're running a Nerd Pub Trivia night!
+      </p>
+      <p>
+        The legendary-ish Nerd Pub Trivia returns this summer for a one time nerdy blow-out.  Recapture the geeky lifestyle of
+        the early 2010's at Boston's least popular trivia night!
+      </p>
+    </div>
+    <div id="schedule">
+      <h1>Where And When?</h1>
+      <p>
+        <a href="http://tommydoyles.com/locations/kendall-square/">Tommy Doyle's</a>
+        in Kendall Square, Cambridge, on Monday, August 11th, at 6:45pm. 
+      </p>
+      <p>
+        We'll be starting at 6:45, but if you arrive a few minutes late we'll make it such that you can catch up.
+        Join us!
+      </p>
+    </div>
+    <div id="footer">
+      <p>
+        <a href="http://twitter.com/NerdPubTrivia">Follow @NerdPubTrivia on Twitter!</a>
+      </p>
+      <p>
+        <a href="http://validator.w3.org/check?uri=referer" target="validator">
+          <img src="/images/valid-html5.png" alt="Valid HTML" height="31" width="88" /></a>
+      </p>
+    </div>
+    <script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-22271758-1']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>
+  </body>
+</html>
